2fc6d12b8075884cfbd33c10ef9d6b8832e41e70
MMA <lesnic1987@mail.ru>
2022-09-04 18:34:08 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] matr)
+{
+    for (int i = 0; i < matr.Length; i++)
+    {
+        matr[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] matr)
+{
+    for (int i = 0; i < matr.Length; i++)
+    {
+        Console.Write($"{matr[i]}, ");
+    }
+}
+
+int[] matrix = new int[8];
+FillArray(matrix);
+PrintArray(matrix);
